f1e7aa16908a7573111162e7f9191b5b173e4ce0
guilherme <guile.hm@hotmail.com>
2018-09-08 20:44:32 -0300

    Update models and migrations for patient and address

--- people/models.py
+++ people/models.py
@@ -1,17 +1,47 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Patient(models.Model):
+    HOLDER = 'holder'
+    DEPENDENT = 'dependent'
+    TYPE_CHOICES = (
+        (HOLDER, 'Holder'),
+        (DEPENDENT, 'Dependent')
+    )
+
+    AFFILIATED = 'affiliated'
+    DISAFFILIATED = 'disaffiliated'
+    ABEYANCE = 'abeyance'
+    STATUS_CHOICES = (
+        (AFFILIATED, 'Affiliated'),
+        (DISAFFILIATED, 'Disaffiliated'),
+        (ABEYANCE, 'Abeyance'),
+    )
+
     registration = models.CharField(max_length=11, db_index=True)
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=200, null=True, blank=True, db_index=True)
     birth_date = models.DateField(db_index=True)
     doc = models.CharField(max_length=20, null=True, blank=True, db_index=True)
-    home_phone_number = models.CharField(max_length=20, null=True)
-    cell_phone_number = models.CharField(max_length=20, null=True)
+    home_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    cell_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES, db_index=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, db_index=True)
+    holder = models.ForeignKey(
+        'people.Patient',
+        related_name='dependents',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_changed = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if self.type == self.DEPENDENT and not self.holder:
+            raise ValidationError('Holder must be selected for dependents.')
 
     def __str__(self):
         return self.name
