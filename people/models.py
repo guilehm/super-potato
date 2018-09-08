47f19443ee2cb415c75c063c62b85de110e82d7e
guilherme <guile.hm@hotmail.com>
2018-09-08 20:19:43 -0300

    Create address model

--- people/models.py
+++ people/models.py
@@ -15,3 +15,19 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Address(models.Model):
+    patient = models.OneToOneField('people.Patient', related_name='address', on_delete=models.CASCADE)
+    postal_code = models.CharField(max_length=10, db_index=True, null=True, blank=True)
+    address = models.CharField(max_length=50)
+    number = models.CharField(max_length=10, null=True, blank=True)
+    complement = models.CharField(max_length=20, null=True, blank=True)
+    district = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.address
