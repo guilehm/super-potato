807279a07d3520c9ba435a8362d891ba7880bdba
guilherme <guile.hm@hotmail.com>
2018-09-08 20:12:49 -0300

    Add date added and changed to Patient

--- people/models.py
+++ people/models.py
@@ -10,5 +10,8 @@ class Patient(models.Model):
     home_phone_number = models.CharField(max_length=20, null=True, blank=True)
     cell_phone_number = models.CharField(max_length=20, null=True, blank=True)
 
+    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_changed = models.DateTimeField(auto_now=True, db_index=True)
+
     def __str__(self):
         return self.name
