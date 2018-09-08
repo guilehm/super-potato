adb757dc04c49c5b54b46f9fe31117e6e8641542
guilherme <guile.hm@hotmail.com>
2018-09-08 20:18:57 -0300

    Update patient model

--- people/models.py
+++ people/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Patient(models.Model):
-    registration = models.CharField(max_length=11)
+    registration = models.CharField(max_length=11, db_index=True)
     name = models.CharField(max_length=40)
-    email = models.EmailField(max_length=200, null=True, blank=True)
-    birth_date = models.DateField()
-    doc = models.CharField(max_length=20)
-    home_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    cell_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True, db_index=True)
+    birth_date = models.DateField(db_index=True)
+    doc = models.CharField(max_length=20, null=True, blank=True, db_index=True)
+    home_phone_number = models.CharField(max_length=20, null=True)
+    cell_phone_number = models.CharField(max_length=20, null=True)
 
-    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
-    date_changed = models.DateTimeField(auto_now=True, db_index=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
