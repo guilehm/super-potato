23ab463c044d1e197af16bbb45456eabf30802ad
Guilherme Luis <guile.hm@hotmail.com>
2018-09-11 23:52:46 -0300

    Add media root and url

--- superPotato/settings.py
+++ superPotato/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'core/media')
+
 
 # Heroku configs
 if os.getcwd() == '/app':
