1d5e3222ba7e5dab0a976dbced575cd3ebf9c556
Guilherme Luis <guile.hm@hotmail.com>
2018-09-12 12:31:28 -0300

    Create covenant admin

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from register.models import Covenant
+
+
+@admin.register(Covenant)
+class CovenantAdmin(admin.ModelAdmin):
+    list_display = ('title', 'doc', 'email', 'status')
+    list_filter = ('status', 'date_added')
+    search_fields = ('title', 'doc', 'email', 'phone_number')
