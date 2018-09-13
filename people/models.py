928a5d011ee13f54f8c54ef388440b08fb245e7e
Guilherme Luis <guile.hm@hotmail.com>
2018-09-13 01:05:17 -0300

    Fix imports

--- people/models.py
+++ people/models.py
@@ -1,5 +1,5 @@
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.functional import cached_property
 
