004fc1c08d168d0551f7baaefced7385eb86fbc1
Guilherme Luis <guile.hm@hotmail.com>
2018-09-13 00:15:04 -0300

    Add debug toolbar urls

--- superPotato/urls.py
+++ superPotato/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from superPotato import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,3 +22,10 @@ urlpatterns = [
     path('', include('core.urls', namespace='core')),
     path('register/', include('register.urls', namespace='register')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
