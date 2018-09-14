4161e29a67caa641333a801f5822d9fd95c9a6be
Guilherme Luis <guile.hm@hotmail.com>
2018-09-13 23:05:40 -0300

    Extends user, register user to django admin, consolidate migrations

--- people/models.py
+++ people/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.functional import cached_property
@@ -18,7 +18,7 @@ class People(models.Model):
     home_phone_number = models.CharField(max_length=20, null=True, blank=True)
     cell_phone_number = models.CharField(max_length=20, null=True, blank=True)
     added_by = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name='created_%(class)s' + 's',
         on_delete=models.SET_NULL,
         null=True,
--- superPotato/settings.py
+++ superPotato/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'register.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
