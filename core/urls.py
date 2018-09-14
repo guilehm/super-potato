356058c27b2cf0d3ca64b4b40c3c24b6c10f03ac
Guilherme Luis <guile.hm@hotmail.com>
2018-09-14 00:26:49 -0300

    Create login and logout views and pages

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
     path('assistance/patient/list/', views.patient_list, name='patient-list'),
 ]
